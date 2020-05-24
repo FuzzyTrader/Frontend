edaba9433e96e71cc89a29bbd8c4bebea81ace32
Clarissa Borges <clarissalimab08@gmail.com>
2020-05-24 20:50:54 -0300

    Creates initial files and Stocks component

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <h1 class="main-title">Fuzzy Trader</h1>
+    <div class="main-container">
+      <Stocks />
+    </div>
+  </div>
+</template>
+
+<script>
+import Stocks from "./components/Stocks.vue";
+
+export default {
+  name: "App",
+  components: {
+    Stocks,
+  },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;700&family=Oswald:wght@400;500;600;700&display=swap");
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
+template {
+  height: 100%;
+}
+
+#app {
+  font-family: Noto Sans, Helvetica, sans-serif;
+  margin: 0;
+  height: 100%;
+}
+
+.main-title {
+  font-family: Oswald, Helvetica, sans-serif;
+  font-weight: bold;
+  font-size: 4rem;
+  color: #05386B;
+  text-align: center;
+}
+
+.main-container {
+  width: 80%;
+  height: 100%;
+  background-color: #5CDB95;
+  border-radius: 16px;
+  box-shadow: 2px 2px 2px 2px rgba(0, 0, 0, 0.5);
+  padding: 32px;
+  text-align: center;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
---
+++ src/components/Stocks.vue
@@ -0,0 +1,119 @@
+<template>
+  <div>
+    <h1 class="welcome">Olá, usuário!</h1>
+    <h2>Quanto você quer investir?</h2>
+    <div class="investment">
+      <h2>USD</h2>
+      <input type="text" id="value" />
+    </div>
+    <div class="stocks">
+      <div class="stocks__card">
+        <h2 class="stocks__card-symbol">ABC</h2>
+        <h1 class="stocks__card-price">17.24</h1>
+        <h2 class="stocks__card-amount">x 5</h2>
+        <h3 class="stocks__card-total">USD 86.20</h3>
+      </div>
+      <div class="stocks__card">
+        <h2 class="stocks__card-symbol">ABC</h2>
+        <h1 class="stocks__card-price">17.24</h1>
+        <h2 class="stocks__card-amount">x 5</h2>
+        <h3 class="stocks__card-total">USD 86.20</h3>
+      </div>
+      <div class="stocks__card">
+        <h2 class="stocks__card-symbol">ABC</h2>
+        <h1 class="stocks__card-price">17.24</h1>
+        <h2 class="stocks__card-amount">x 5</h2>
+        <h3 class="stocks__card-total">USD 86.20</h3>
+      </div>
+      <div class="stocks__card">
+        <h2 class="stocks__card-symbol">ABC</h2>
+        <h1 class="stocks__card-price">17.24</h1>
+        <h2 class="stocks__card-amount">x 5</h2>
+        <h3 class="stocks__card-total">USD 86.20</h3>
+      </div>
+    </div>
+    <h1 id="total">Total: USD 431.00</h1>
+    <button id="buy-stocks">Comprar ativos</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Stocks",
+};
+</script>
+
+<style scoped>
+h1,
+h2,
+h3 {
+  font-weight: 400;
+}
+
+.welcome {
+  text-align: left;
+}
+
+.investment {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+input {
+  margin: 0 8px;
+  padding: 16px;
+  width: 8rem;
+  border-radius: 0;
+  border: 2px solid #05386B;
+  background-color: #EDF5E1;
+  font-size: 1.275rem;
+}
+
+input:focus {
+  border: 3px solid #05386B;
+}
+
+.stocks {
+  display: flex;
+  justify-content: center;
+}
+
+.stocks__card {
+  background-color: #EDF5E1;
+  border: 3px solid #05386B;
+  margin: 16px;
+  width: 20%;
+  padding: 0 8px;
+}
+
+.stocks__card-symbol,
+.stocks__card-total {
+  background-color: #05386B;
+  height: 15%;
+  color: #EDF5E1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+button {
+  display: flex;
+  margin-left: auto;
+  background-color: #05386B;
+  border: 3px solid #05386B;
+  padding: 16px;
+  border-radius: 8px;
+  cursor: pointer;
+  text-transform: uppercase;
+  font-weight: bold;
+  color: #EDF5E1;
+}
+
+button:hover,
+button:active {
+  background-color: transparent;
+  color: #05386B;
+}
+
+</style>
